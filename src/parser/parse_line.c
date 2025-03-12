bfeb6e21f61c7ead67a5802e783fdff3e930f113
Bard-Gaming <christophe.dronne@epitech.eu>
2025-03-12 09:57:28 +0100

    [src/parser] Updated parser to clean everything up and return NULL when a syntax error is caught

--- src/parser/parse_line.c
+++ src/parser/parse_line.c
@@ -10,6 +10,32 @@
 #include <42parser/lexer.h>
 
 
+static bool is_operation(const ast_t *ast)
+{
+    return
+        AT_COMMAND_CHAIN <= ast->type && ast->type <= AT_OPERATION_OR;
+}
+
+static bool has_error(const ast_t *ast)
+{
+    ast_command_t *data;
+
+    if (ast->type == AT_ERROR)
+        return true;
+    if (is_operation(ast))
+        return
+            has_error(((ast_t **)ast->data)[0]) ||
+            has_error(((ast_t **)ast->data)[1]);
+    return false;
+}
+
+/*
+** Parses a line into an abstract syntax
+** tree.
+** Returns NULL if a syntax error was caught.
+** The given ast should be deleted with
+** ast_delete once it isn't needed anymore.
+*/
 ast_t *parse_line(const char *line)
 {
     ast_t *result;
@@ -19,5 +45,8 @@ ast_t *parse_line(const char *line)
     parser_init(&parser);
     result = parse_statement(&parser);
     parser_term(&parser);
-    return result;
+    if (!has_error(result))
+        return result;
+    ast_delete(result);
+    return NULL;
 }
