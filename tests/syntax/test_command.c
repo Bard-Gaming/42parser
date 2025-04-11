7b253d04a3c11adaa4dba9bc6e3d17a79371597a
Bard-Gaming <christophe@dronne.fr>
2025-04-11 12:54:17 +0200

    [tests/syntax] Added string-related tests to command syntax tests

--- tests/syntax/test_command.c
+++ tests/syntax/test_command.c
@@ -50,3 +50,48 @@ Test(test_command, multiple_commands_sc)
     cr_assert_neq(ast, NULL);
     cr_assert_eq(P_ERRNO, PE_NONE);
 }
+
+Test(test_command, raw_string)
+{
+    ast_t *ast;
+    const char *input = "echo 'hewwo :3'";
+
+    ast = parse_input(input);
+    cr_assert_neq(ast, NULL);
+    cr_assert_eq(P_ERRNO, PE_NONE);
+}
+
+Test(test_command, format_string)
+{
+    ast_t *ast;
+    const char *input = "echo \"hewwo :3\"";
+
+    ast = parse_input(input);
+    cr_assert_neq(ast, NULL);
+    cr_assert_eq(P_ERRNO, PE_NONE);
+}
+
+//////////////////////////////////////////////////
+//                                              //
+//                 FAILING TESTS                //
+//                                              //
+//////////////////////////////////////////////////
+Test(test_command, unmatched_raw_string)
+{
+    ast_t *ast;
+    const char *input = "echo 'hello\n";
+
+    ast = parse_input(input);
+    cr_assert_eq(ast, NULL);
+    cr_assert_eq(P_ERRNO, PE_UNMATCHED_RAW_STRING);
+}
+
+Test(test_command, unmatched_format_string)
+{
+    ast_t *ast;
+    const char *input = "echo \"hello";
+
+    ast = parse_input(input);
+    cr_assert_eq(ast, NULL);
+    cr_assert_eq(P_ERRNO, PE_UNMATCHED_FORMAT_STRING);
+}
