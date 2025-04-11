333ac66aaab983c9c2badedbeab3b8cb666b4714
Bard-Gaming <christophe@dronne.fr>
2025-04-12 00:42:11 +0200

    [tests/syntax] Added unit tests for command compounds

---
+++ tests/syntax/test_compounds.c
@@ -0,0 +1,54 @@
+/*
+** EPITECH PROJECT, 2025
+** Project - 42parser
+** File description:
+** Unit tests for command
+** compound syntax.
+*/
+
+#include <criterion/criterion.h>
+#include <42parser/parser.h>
+#include <42parser/error.h>
+
+
+//////////////////////////////////////////////////
+//                                              //
+//                 PASSING TESTS                //
+//                                              //
+//////////////////////////////////////////////////
+Test(test_compounds, simple_compound)
+{
+    ast_t *ast;
+    const char *input = "(command_a; command_b;;; command_c)";
+
+    ast = parse_input(input);
+    cr_assert_neq(ast, NULL);
+    cr_assert_eq(P_ERRNO, PE_NONE);
+}
+
+//////////////////////////////////////////////////
+//                                              //
+//                 FAILING TESTS                //
+//                                              //
+//////////////////////////////////////////////////
+Test(test_compounds, unmatched_parenthesis_missing)
+{
+    ast_t *ast;
+    const char *input = "(command_a; command_b;;;";
+
+    ast = parse_input(input);
+    cr_assert_eq(ast, NULL);
+    cr_assert_eq(P_ERRNO, PE_UNMATCHED_PARENTHESIS);
+}
+
+Test(test_compounds, unmatched_parenthesis_newline)
+{
+    ast_t *ast;
+    const char *input =
+        "(command_a\n"
+        "command_b;;; command_c)";
+
+    ast = parse_input(input);
+    cr_assert_eq(ast, NULL);
+    cr_assert_eq(P_ERRNO, PE_UNMATCHED_PARENTHESIS);
+}
